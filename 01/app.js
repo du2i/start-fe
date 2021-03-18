723cfa6ed9f1f1b59d96bf6a06e506b72b83de34
du2i <lucy051642@gmail.com>
2021-03-18 15:54:32 +0900

    01 수업과제

---
+++ 01/app.js
@@ -0,0 +1,35 @@
+console.log('app.js');
+
+var image = {
+
+    width : 100,
+    height : 100,
+    name:'1.png',
+};
+
+// var image1 =[1,2,3,4,5,6,'a',{}];
+// image1[0]='a';
+
+// var date = null;
+// var data1 = '';
+// var data2;
+
+// console.log(data, data1, data2);
+// var imageWidth = 100;
+// var imageHeight = 100;
+// varimageName = '1.png';
+
+// // console.log(image,image1);
+// var count =1;
+// var number ='2';
+
+// console.log(count+number);
+
+// console.log(1 == 1);
+
+// if(true){
+//     console.log(1);
+
+// }
+console.log(typeof 1);
+
