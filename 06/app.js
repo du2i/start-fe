2df444b8e195b9a3567ccf5da4a17daf1deccd50
du2i <lucy051642@gmail.com>
2021-04-15 15:38:00 +0900

    06

---
+++ 06/app.js
@@ -0,0 +1,33 @@
+// let a = 'aa';
+// const b = 'bb';
+// var c = '11';
+// c =33;
+// console.log(a,b,c);
+
+// var str = 'aaaaaaa \
+// ssdsdf ' ;
+// const s =  `aaaaa
+// cccc
+// ddd `;
+// console.log(str,s);
+ 
+// function sum(x,y = 1) {
+//     return x + y;
+// }
+// console.log(sum(1));
+
+const arr = [1,2,3];
+
+const newArr = arr.map((item) => item + 1);
+console.log(newArr);
+
+document
+.getElementById('box')
+.addEventListener('click', event=> console.log(1));
+
+function sum(x,y,x){
+    return x + y + z;
+}
+const words = ['a', 'b'];
+const newWords = [...words];
+console.log(newWords);
